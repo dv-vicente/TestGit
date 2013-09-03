6562d48a3bd55cd8f012acc60bbc309c81419ceb
dv-vicente <dv.vicente@gmail.com>
2013-09-03 09:59:36 +0200

    Hello Git

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dvicente
+ * Date: 3/09/13
+ * Time: 9:57
+ */
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("Hello Git...");
+    }
+}
